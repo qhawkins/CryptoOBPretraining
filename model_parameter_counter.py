c8e069d302975179ddb624a61260556b64ae8e74
qhawkins <qhawkins2002@gmail.com>
2025-01-17 16:53:29 -0500

    deep narrow model

--- model_parameter_counter.py
+++ model_parameter_counter.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-from fp8_models import TinyTransformerModel
+from fp8_models import TinyTransformerModel, DeepNarrowTransformerModel
 
 def count_parameters(model: torch.nn.Module):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     #tiny_lstm = TinyLSTMModel((128, 128, 2), (128, 128, 2), 0.25)
     #small_lstm = ShallowLSTMModel((128, 128, 2), (128, 128, 2), 0.25)
     #deep_lstm = DeepLSTMModel((128, 128, 2), (128, 128, 2), 0.25)
-    tiny_transformer = TinyTransformerModel((128, 92, 2), (128, 92, 2), 0.25)
+    tiny_transformer = DeepNarrowTransformerModel((256, 96, 2), (256, 96, 2), 0.0)
     #print(f"Small model has {count_parameters(small)/1e6} M parameters")
     #print(f"Medium model has {count_parameters(medium)/1e6} M parameters")
     #print(f"Large model has {count_parameters(large)/1e6} M parameters")
